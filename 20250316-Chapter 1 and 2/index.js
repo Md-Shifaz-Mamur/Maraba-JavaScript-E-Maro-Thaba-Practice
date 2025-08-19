7e1ff37fd051bdd0e3d73c81ae9a89bfdf1f4dd5
Md-Shifaz-Mamur <shifazmamur7@gmail.com>
2025-08-20 00:08:49 +0600

    first commit

---
+++ 20250316-Chapter 1 and 2/index.js
@@ -0,0 +1,340 @@
+// console.log("Hello Javascript!!");
+// console.log("I Love Javascript");
+// console.log("Ya Allah, give me the strength to finish Javascript")
+
+// const age = 45;
+// console.log(age);
+
+// const booksToRead = 20;
+// console.log(booksToRead);
+
+// const wakeUpTime = 5.00;
+// console.log(wakeUpTime);
+
+// const wastedTime = 2;
+// console.log(wastedTime);
+
+// const borrow = 100;
+// console.log(borrow);
+
+// const goals = "7 UP";
+// console.log(goals);
+
+// const pillow = 3;
+// console.log(pillow);
+
+// const name = 'Shifaz Mamur';
+// console.log(name);
+
+// const address = 'Mohammadia housing ltd, MOhammadpur, Dhaka, 1207';
+// console.log(address);
+
+// const university = 'Khulna University';
+// console.log(university);
+
+// const fan = 'None';
+// console.log(fan);
+
+// const movieOfTheYear = 'No Movie';
+// console.log(movieOfTheYear);
+
+// const hobby = 'Singing';
+// console.log(hobby);
+
+// const color = 'White';
+// console.log(color);
+
+// const happy = false;
+// console.log(happy);
+
+// const message = false;
+// console.log(message);
+
+// const celebrating = true;
+// console.log(celebrating);
+
+// const isHappy = true;
+// const hasMoney = false;
+// const isHungry = true;
+// console.log(isHappy);
+// console.log(hasMoney);
+// console.log(isHungry);
+
+
+// const isStudyDone = false;
+// console.log(isStudyDone);
+
+// const isPhoneCharged = false;
+// console.log(isPhoneCharged);
+
+// const isDinnerDone = true;
+// console.log(isDinnerDone);
+
+
+
+
+// /*
+// const eggPrice = 10;
+// const onionPrice = 20;
+// const result = eggPrice - onionPrice;
+// console.log(result);
+// */
+
+// // const eggPrice = 10;
+// // const onionPrice = 20;
+// // console.log(onionPrice - eggPrice);
+
+// const eggPrice = 10;
+// const onionPrice = 20;
+// console.log(eggPrice * onionPrice);
+// console.log(eggPrice/onionPrice);
+// console.log(onionPrice/eggPrice);
+// console.log(onionPrice%eggPrice);
+
+// console.log(11%7);
+// console.log(10%3);
+
+// const first = "Mobarok";
+// const second = "Tobarok";
+// console.log(first+second);
+
+// console.log(4**2);
+
+// const sugar = 20;
+// const daddy = '20';
+// console.log(sugar+daddy);
+
+// // Practice: Page 47
+
+// // ১
+// const earn = 100
+// const cost = 40
+// const remaining = earn-cost;
+// console.log(remaining);
+
+// // ২
+// const pencilInPossession = 10;
+// const useAtOnce = 2;
+// const timeUsable = pencilInPossession/useAtOnce;
+// console.log(timeUsable);
+
+// // ৩
+// const moneyHas = 60;
+// const spendEachTime = 15;
+// const timeCanBeUsed = moneyHas/spendEachTime;
+// console.log(timeCanBeUsed);
+// const remainingEveryTimeSpend = moneyHas-spendEachTime;
+// console.log(remainingEveryTimeSpend);
+
+// // ৪
+// const firstWord = "Hello";
+// const secondWord = "World";
+// console.log(firstWord+secondWord);
+
+// // ৫
+// const rice = 153;
+// const pulse = 261;
+// const totalInKg = rice+pulse;
+// console.log(totalInKg)
+
+// // ৬
+// const initialMoney = 500;
+// const spendAtOnce = 75;
+// const abilityOfSpend = initialMoney/spendAtOnce;
+// console.log(abilityOfSpend);
+// const remainder = initialMoney%spendAtOnce;
+// console.log(remainder);
+
+// // ৭
+// const dividend = 8;
+// const dividor = 3;
+// const quotient = dividend/dividor;
+// const remainder7 = dividend%dividor;
+// console.log(quotient);
+// console.log(remainder7);
+
+// // ৮
+// const dividend8 = 50;
+// const dividor8 = 9;
+// const remainder8 = dividend8%dividor8;
+// console.log(remainder8);
+
+// // ৯
+// const firstName = 'Bangla';
+// const lastName = 'desh';
+// console.log(firstName+lastName);
+
+// // ১০
+
+// const dailyIntakeKg = 4;
+// const riceProvisionKg = 480;
+
+// // 4 kg = 1 day 
+// // 1 kg = 1/4 day 
+// // 480 kg = {(1*480)/4} day
+
+// const totalSustenanceInDays = ((1*riceProvisionKg)/dailyIntakeKg);
+// console.log(totalSustenanceInDays);
+
+
+
+// const totalSustenanceInMonths = totalSustenanceInDays/30;
+// console.log(totalSustenanceInMonths);
+
+// // Output: 120 Days , 4 Months
+
+
+
+// Chapter 2 
+// Practice: Page 35  
+
+const dateOfBirth = 1996;
+const siblings = 4;
+const students = 45;
+const marks = 80;
+const rooms = 5;
+const study = 12;
+const total = 52;
+
+// Console.log 
+// Page 37 
+
+console.log(dateOfBirth);
+console.log(siblings);
+console.log(students);
+console.log(marks);
+console.log(rooms);
+console.log(study);
+console.log(total);
+
+
+// Practice: Page 38
+const age = 28;
+const booksToRead = 6;
+const wakeUpTime = '5 AM';
+const wastedTime = '2 hours';
+const borrow = 0;
+const goals = 7;
+const pillow = 2;
+console.log(age);
+console.log(booksToRead);
+console.log(wakeUpTime);
+console.log(wastedTime);
+console.log(borrow);
+console.log(goals);
+console.log(pillow);
+
+
+
+// Practice: Page 41
+// String Variable 
+const myName = 'Md Shifaz Mamur';
+const address = 'Mohammadia Housing Ltd., Mohammadpur, Dhaka 1207';
+const institute = 'Khulna University';
+const fan = 'none';
+const movieOfTheYear = 'none';
+const hobby = 'Programming';
+const color = 'White';
+console.log(myName);
+console.log(address);
+console.log(institute);
+console.log(fan);
+console.log(movieOfTheYear);
+console.log(hobby);
+console.log(color);
+
+// Page 42/43 
+const happy = false;
+const message = true;
+const celebrating = true;
+const isHappy = true;
+const hasMoney = false;
+const isHungry = true;
+const isStudyDone = false;
+const isPhoneCharged = false;
+const isDinnerDone = false;
+console.log(happy);
+console.log(message);
+console.log(celebrating);
+console.log(isHappy);
+console.log(hasMoney);
+console.log(isHungry);
+console.log(isStudyDone);
+console.log(isPhoneCharged);
+console.log(isDinnerDone);
+
+// Page 47 
+// Practice Problem 
+
+// 1  Ans: 60
+const earn = 100;
+const spent = 40;
+const remaining = earn - spent;
+console.log(remaining);
+
+// 2  Ans: 5
+const pencilInPossession = 10;
+const pencilUsedInOneTime = 2;
+const pencilUsedOverallResult = pencilInPossession / pencilUsedInOneTime;
+console.log(pencilUsedOverallResult);
+
+// 3  Ans: 4; Ans: 0
+const takaInHand = 60;
+const spendEachTime = 15;
+const  takaUsedOverallResult = 60/15;
+const remainingAfterAllSpending = takaInHand % spendEachTime;
+console.log(takaUsedOverallResult);
+console.log(remainingAfterAllSpending);
+
+// 4
+const Hello = 'Hello';
+const World = 'World';
+const helloWorldAdded = Hello+World;
+console.log(helloWorldAdded);
+
+
+// 5 
+const rice = 153;
+const pulse = 261;
+const totalGroceryShopping = rice + pulse;
+console.log(totalGroceryShopping);
+
+// 6 
+const firstAmount = 500;
+const amountSpentAtOnce = 75;
+const spendQuota = firstAmount/amountSpentAtOnce;
+const remainingAfterISpend = firstAmount % amountSpentAtOnce;
+console.log(spendQuota);
+console.log(remainingAfterISpend);
+
+// 7 
+const numerator1 = 8;
+const denominator1 = 3;
+const result1 = numerator1/denominator1;
+const remainder1 =  numerator1%denominator1;
+console.log(result1);
+console.log(remainder1);
+
+
+// 8 
+const numerator2 = 50;
+const denominator2 = 9;
+const result2 = numerator2/denominator2;
+const remainder2 =  numerator2%denominator2;
+console.log(result2);
+console.log(remainder2);
+
+
+// 9 
+const Bangla = 'Bangla';
+const desh = 'desh';
+const Bangladesh = Bangla+desh;
+console.log(Bangladesh);
+
+// 10 
+const dailyIntakeRiceKg = 4; /* in kg */
+const totalRiceGiven = 480; /* in kg */
+const totalDaysOfSustenance = totalRiceGiven/dailyIntakeRiceKg;
+const totalSustenanceInMonths = totalDaysOfSustenance/30;
+console.log(totalDaysOfSustenance);
+console.log(totalSustenanceInMonths);
