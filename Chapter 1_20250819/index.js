3de10f9c9a78ebccec797ab5b914bb08cdee5923
Md-Shifaz-Mamur <shifazmamur7@gmail.com>
2025-08-20 15:59:56 +0600

    4th commit

---
+++ Chapter 1_20250819/index.js
@@ -0,0 +1,47 @@
+// // Practice  
+// // Page: 24 - 26
+// // 1
+// Given = -5, 12, -8, 0, 3.5
+// Positive = 12, 0, 3.5
+// Negative = -5, -8
+
+// // 2 
+// Given = 23, 56, 19.69, 101, 42, 89, 11.37
+// Integer = 23, 56, 101, 42, 89
+// Even = 56, 42 
+// Odd = 23, 101, 19.69, 89, 11.37
+
+// // 3 
+// Given = 3.4, 7/8, 2.75, 1/3
+// Decimal = 3.4, 2.75
+// Fraction = 7/8, 1/3
+
+// // 4 
+// 14 + 21 + 55 = 90
+// 32 + 47 - 25 = 54
+
+// // 5 
+// 100 - 64 = 36
+
+// // 6 
+// Total cost = 5*350 = 1750
+
+// // 7
+// // Everyone gets = 450 / 8 = 56.25 BDT 
+// const remainder = 450%8; /* 2 */
+// console.log(remainder);
+
+// // 8 
+// 15 > 12 = correct
+// 4 < 10 = correct
+// 7 > 9 = incorrect
+
+// // 9 
+// Average of 20, 25, 30 and 91 is 41.5
+
+// 10 
+const pie = 3.1416
+const r = 7;
+const area = pie*(r ** 2);
+console.log(area);
+
---
+++ Chapter 3_20250317/index.js
@@ -0,0 +1,265 @@
+// // Data Type 
+
+// // Command: typeof = Shows data type
+
+// const passed = false;
+// console.log(typeof false);
+
+// const country = "Bangladesh";
+// const pass = true;
+
+
+// console.log(typeof country);
+// console.log(typeof pass);
+// console.log(typeof price);
+
+
+// Page 50 - Practice
+
+// ১
+// const price = 555;
+// console.log(typeof price);
+
+// // ২
+
+// const learning = true;
+// console.log(typeof learning);
+
+// // ৩
+// const megaCity = 'Dhaka';
+// console.log(typeof megaCity);
+
+// // ৪
+// const temperature = 102.5;
+// console.log(typeof temperature);
+
+// // ৫
+// const isHungry = true
+// console.log(typeof isHungry)
+
+// // ৬
+// const isReach = true 
+// console.log(typeof isReach)
+
+
+// // Let দিয়ে শুরু, বদলাবে গুরু
+// let price1 = 34
+// price1 = 45
+// console.log(price1)
+
+// let name = 'lalmia'
+// name = 'kalamia'
+// console.log(name);
+
+// // Page 52 - Practice
+// // 1
+// let age = 28;
+// age = 30;
+// console.log(age);
+
+// // 2
+// let pocketMoney = 500;
+// pocketMoney = 150;
+// console.log(pocketMoney);
+
+// // 3
+// let studyTime = 4
+// studyTime = 6
+// console.log(studyTime);
+
+// // 4
+// let friendsCount = 3
+// friendsCount = 1
+// console.log(friendsCount);
+
+// // 5
+// let phonePrice = 25000
+// phonePrice = 12000
+// console.log(phonePrice);
+
+// // 6
+// let chairsCount = 40
+// chairsCount = 38
+// chairsCount = 32
+// console.log(chairsCount);
+
+// // 7
+// let playTime = 3
+// playTime = 0
+// console.log(playTime); 
+
+// // Page 56 - Practice 
+// // Comment 
+
+// // 1
+// // Math Final Exam Score (Out of 100)
+// const score = 92;
+
+// // 2
+// // Radius of the field where we are playing.
+// // It is really important to understand the radius.
+// // Here, the radius is measured in meters.
+// const radius = 40;
+
+// // 3
+// /*
+// This variable shows the number of my favorite shirts. 
+// This is always important to have some special clothes for different occasions. 
+// The variable representing number of shirts is declared as dress.   
+// */ 
+
+// let dress = 6;
+
+
+// // 4
+// // Mom gifted me for getting highest score in the term final exam.
+
+// const gift = 100; 
+
+// let degree = 'MSS'
+// degree = 'PhD'
+// console.log(degree);
+
+
+// 20 August, 2025
+const passed = false;
+console.log(typeof passed);
+
+// Data Type
+// 1. NUmber 2. String 3.Boolean
+
+// Practice: Page 50
+const price = 555;
+const learning = true;
+const megaCity = 'Dhaka';
+const temperature = 102.5;
+const isHungry = true;
+const isReach = true;
+console.log(typeof price);
+console.log(typeof learning);
+console.log(typeof megaCity);
+console.log(typeof temperature);
+console.log(typeof isHungry);
+console.log(typeof isReach);
+
+// Let দিয়ে শুরু, বদলাবে গুরু
+// Page 52 - 53
+
+// 1
+let age = 28;
+age = 29
+
+// 2
+let pocketMoney = 500;
+pocketMoney = 150;
+
+// 3
+let studyTime = 4;
+studyTime = 6;
+
+// 4
+let friendsCount = 3;
+friendsCount = 1;
+
+// 5
+let phonePrice = 25000;
+phonePrice = 12000;
+
+// 6
+let chairsCount = 40;
+chairsCount = 38;
+chairsCount = 32;
+
+// 7
+let playTime = 3;
+playTime = 0;
+
+console.log(age);
+console.log(pocketMoney);
+console.log(studyTime);
+console.log(friendsCount);
+console.log(phonePrice);
+console.log(chairsCount);
+console.log(playTime);
+
+
+// Page 56
+// Comment 
+
+// 1. Math score in 1-1 Term Final Exam 
+const score = 89;
+
+// 2. Border Radius
+// Helps to make the card look nicer 
+// 100% Required 
+const radius = '10px';
+
+/*3. Number of Favourite Dress 
+Eid ul fitr 2025 
+Dress Color: Blue, white, purple */
+const dress = 5;
+
+// 4. Gift from Mother
+// Gift money in BDT 
+// Gift for today's grocery and vegetables shopping 
+const giftMoney = 100;
+
+// 5. I will be studying for 7 hours.
+// Quran = 1
+// Programming = 4 hours
+// Seerah = 2 hours
+// May Allah help me
+const studyTime1 = 7;
+
+// 6. send money to Asif by bKash = 2000 tk 
+// service fee = 28 tk
+const sendMoney = 2000;
+
+// 7. Pay for universal travel adapter 
+// product price 800, delivery fee 60
+const pay = 860;
+
+console.log(score);
+console.log(radius);
+console.log(dress);
+console.log(giftMoney);
+console.log(studyTime1);
+console.log(sendMoney);
+console.log(pay); 
+
+
+// Practice: Page: 59 - 60
+// 1.
+const favoriteFoodOfMine = 'Biriyani';
+console.log(favoriteFoodOfMine);
+
+// 2.
+const city = 'Chittagong';
+const City = 'Dhaka';
+console.log(city);
+console.log(City);
+
+// 3.
+const userName = 'Johan';
+const username = 'Jovan';
+console.log(userName);
+console.log(username);
+
+// 4.
+const myHouseAddress = "Mohammadia Housing Ltd.";
+console.log(myHouseAddress);
+
+// 5.
+const super = 24;
+console.log(super);
+
+// 6.
+const class = 17;
+console.log(class);
+
+// 7.
+const 2ndPosition = 'Imrul Kayes'; //Correct form: position2nd or positionSecond 
+console.log(2ndPosition);
+
+
+
