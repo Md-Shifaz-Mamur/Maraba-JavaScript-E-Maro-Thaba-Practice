4bdebdc99d9e3f56ab700c163d520c5e97bef1e2
Md-Shifaz-Mamur <shifazmamur7@gmail.com>
2025-08-22 20:15:28 +0600

    chapter 4 ongoing

---
+++ Chapter 4_20250820/index.js
@@ -0,0 +1,50 @@
+// Chapter 4
+// Comparison 
+const test = 12;
+console.log(test);
+
+console.log(5<10);
+console.log(5>10);
+
+console.log(5==10);
+console.log(5!=10);
+
+console.log(10<=10);
+
+// Practice - Page 68 
+// 1
+const shifazEats = 5;
+const rakibEats = 7;
+console.log(rakibEats>shifazEats);
+
+// 2
+const myClass = 45;
+const nextClass = 50;
+console.log(myClass<nextClass);
+
+// 3
+const myNumber = 75;
+const hisNumber = 75;
+console.log(myNumber == hisNumber);
+
+// 4
+const savingsTarget = 100;
+const currentSavings = 95;
+console.log(currentSavings<savingsTarget); 
+
+// 5
+console.log(10!=7);
+
+// 6
+console.log(20<=15);
+
+// 7
+console.log(97<45);
+
+
+// Practice - Page 70-71
+// 1
+const youngerBro = 10;
+const me = 15;
+console.log(youngerBro<=me);
+
