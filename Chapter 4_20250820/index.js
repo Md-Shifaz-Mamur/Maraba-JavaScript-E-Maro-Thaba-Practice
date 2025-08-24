ed05aaa8c33836a000861aa5d33c4f4cb38b23f8
Md-Shifaz-Mamur <shifazmamur7@gmail.com>
2025-08-24 22:26:14 +0600

    4th chapter done

--- Chapter 4_20250820/index.js
+++ Chapter 4_20250820/index.js
@@ -48,3 +48,88 @@ const youngerBro = 10;
 const me = 15;
 console.log(youngerBro<=me);
 
+// 2
+const passMark = 33;
+const obtainedMark = 45;
+console.log(obtainedMark>=passMark);
+
+// 3
+const jambura = 100;
+const kodbel = 80;
+console.log(jambura>kodbel);
+
+
+
+//4
+const meRead = 10;
+const friendRead = 10;
+console.log(meRead==friendRead);
+
+//5
+const meBiscuit = 15;
+const friendBiscuit = 20;
+console.log(meBiscuit<friendBiscuit);
+
+//6
+const realMadrid = 2;
+const barcelona = 3;
+console.log(realMadrid<barcelona);
+
+//7
+const meName = "Abdur Rahim";
+const frdName = "Abdul Karim";
+console.log(meName==frdName);
+
+//8
+const iStudy = true;
+const friendStudy = false;
+console.log(iStudy==friendStudy);
+
+
+// Practice: Page 73
+
+//1
+console.log(50=="50");
+console.log(50==="50");
+
+//2
+console.log("JavaScript"=='JavaScript');
+console.log("JavaScript"==='JavaScript');
+
+/*Both of the outputs are true because string variables in single or double quotation mark, gives the same output and same data type as well.*/
+
+//3
+console.log(25=="25");
+/*This will show True as output because '==' does not consider data type and hence it only gives the output based on Value. As the values are same, it shows True.*/
+
+
+console.log(25==="25");
+
+/*It will show 'False' as output because now '===' considers both the value and data type. As there are 2 different data types, 25 = number and "25" = string, therefore the output is shown "False".*/
+
+// 4
+console.log("apple"=="apple");
+console.log("apple"==="apple");
+
+/*Both are true as data type and variable are all the same in both cases.*/
+
+
+// 5
+console.log("test"=="TEST", "test"==="TEST");
+
+// 6
+const variableX = 15;
+const variableY = "20"; 
+console.log(variableX!=variableY,variableX!==variableY);
+
+// 7
+// const variableA = hello; //It will show error message 'cause string type variables cannot be written without single or double quotations 
+
+const variableA = "hello"; //correct way 
+const variableB = "Hello";
+console.log(variableA==variableB, variableA===variableB);
+
+
+
+
+
