2dd11f7fa33f72d8aa1cb8afbdde5cf910fbc34b
Md-Shifaz-Mamur <shifazmamur7@gmail.com>
2025-08-27 20:17:25 +0600

    6th chapter starts

---
+++ Chapter 6_20250827/index.js
@@ -0,0 +1,69 @@
+// Chapter 6
+// Array 
+
+const numbers = [12, 34, 141, 43];
+
+// Practice: Page 93 
+// 1
+const array1 = [71, 72, 73, 74, 75, 76, 77, 78, 79];
+
+// 2
+const fruits = ['apple', 'banana', 'orange', 'mango', 'guava'];
+
+// 3
+const vegetables = ['potato', 'onion', 'brinjal', 'radish', 'carrot', 'tomato',  'okra', 'gourd', 'cucumber', 'pointed gourd']
+
+// 4
+const movies = ['movie 1', 'movie 2','movie 3','movie 4','movie 5', ]
+
+// 5
+const odds = [11, 13, 15, 17, 19, 21, 23, 25, 27, 29];
+
+// 6
+const color = ['black', 'yellow', 'white', 'blue', 'orange', 'green'];
+console.log(color.length);
+const third = color[3];
+console.log(third);
+
+// 7
+const capitalCities = ['Dhaka', 'New Delhi', 'Doha', 'Islamabad', 'Cairo'];
+console.log(capitalCities[3]);
+console.log(capitalCities.length);
+
+// Index = Position inside array 
+
+// Practice
+// Page: 96
+
+// 1
+const friends = ['sahnto', 'panto', 'ranto', 'jyanto','onto'];
+console.log(friends[3]);
+
+// 2
+const favBooks = ['book 1', 'book 2','book 3','book 4','book 5','book 6','book 7',];
+console.log(favBooks[5]);
+
+// 3
+const numberArray = [1,2,3,4,5,6,7,8,9,10];
+numberArray[7] = 30;
+console.log(numberArray);
+
+// 4
+const games = ['free fire', 'pub-g', 'candy crush', 'temple run'];
+games[2]='subway surfer';
+console.log(games);
+
+// 5
+const family = ['abbu', 'ammu', 'ezaz', 'akhi', 'niaz', 'shifaz'];
+console.log(family[3]);
+
+// 6
+const countriesToTravel = ['KSA', 'UAE', 'USA', 'Turkiye', 'UK', 'Qatar', 'Palestine'];
+console.log(countriesToTravel);
+console.log(countriesToTravel[4]);
+console.log(countriesToTravel.length);
+
+// 7
+const table = ['laptop', 'desktop', 'printer', 'book'];
+console.log(table[7]);
+
