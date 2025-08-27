40a28ab7594c954db848cc306252b458e0ca5c99
Md-Shifaz-Mamur <shifazmamur7@gmail.com>
2025-08-27 17:45:46 +0600

    Chapter 5 Done

--- Chapter 5_20250824/index.js
+++ Chapter 5_20250824/index.js
@@ -230,20 +230,183 @@ if (bookPageNumbers < 100) {
 
 // 6
 const waterTemperature = 22;
-if(waterTemperature<0){
+if (waterTemperature < 0) {
     console.log("Ice");
-} else if (waterTemperature>=0 && waterTemperature<=20){
+} else if (waterTemperature >= 0 && waterTemperature <= 20) {
     console.log("Cool Cool");
-} else if (waterTemperature>20){
+} else if (waterTemperature > 20) {
     console.log("Hot Hot!!!");
 }
 
 // 7
 const playerLevel = 56;
-if(playerLevel<10){
+if (playerLevel < 10) {
     console.log("Novice")
-} else if (playerLevel>=10 && playerLevel<=50){
+} else if (playerLevel >= 10 && playerLevel <= 50) {
     console.log("Expert");
-} else if (playerLevel>50){
+} else if (playerLevel > 50) {
     console.log("Pro Gamer!!!");
 }
+
+
+// Nested if-else 
+const moneyInHand = 400;
+const popCornPrice = 40;
+if (moneyInHand > 300) {
+    console.log("Tui cinema dekhte jabi!");
+    if (popCornPrice <= 50) {
+        console.log("Popcorn Kinbi");
+    } else {
+        console.log("Popcorn is expensive");
+    }
+
+} else {
+    console.log("Basay bose thakbi!");
+}
+
+
+// Practice: Page 86 
+// 1
+const friendInvite = true;
+const moneyinPocket = 1500;
+if (friendInvite == true) {
+    console.log("Attend birthday party!");
+
+    if (moneyinPocket > 1000) {
+        console.log("Buy a gift for friend!");
+    } else {
+        console.log("Attend party empty handed!");
+    }
+
+} else {
+    console.log("Exclude from the friend list!");
+}
+
+// 2
+const guest = true;
+const drinkTea = true;
+if (guest == true) {
+    console.log("Do u want to have a cup of tea?");
+
+    if (drinkTea == true) {
+        console.log("sathe biscuit khaben?");
+    } else {
+        console.log("Ok, sudhu chaa ready!")
+    }
+
+} else {
+    console.log("Watch Star Jalsa on TV!");
+}
+
+// 3
+const accActivated = false;
+const premiumAccount = false;
+if (accActivated == true) {
+    console.log("Check subscription!");
+
+    if (premiumAccount == true) {
+        console.log("Show Premium Features!");
+    } else {
+        console.log("Watch free version!");
+    }
+
+} else {
+    console.log("Account is not activated!");
+}
+
+// 4
+const foodInFridge = false;
+const foodDeliveryApp = false;
+if (foodInFridge == true) {
+    console.log("Khabar gorom korbi!");
+
+} else if (foodDeliveryApp == true) {
+    console.log("Order Food");
+} else {
+    console.log("Ami Ajke roja");
+}
+
+
+// 5
+const partyGuests = 99;
+const bringGift = false;
+if (partyGuests > 100) {
+    console.log("Check whether all bring gifts along with them");
+    if (bringGift == true) {
+        console.log("Let's party all night");
+    } else {
+        console.log("I will party with myself!");
+    }
+
+} else {
+    console.log("No Party At All");
+}
+
+// Ternary Operator 
+const age12 = 34;
+// if(age12>=18){
+//     console.log("Vote now");
+// } else {
+//     console.log("You are not eligible to vote");
+// }
+
+console.log(age12 >= 18 ? "Eligible" : "Not eligible");
+
+
+let prodPrice = 500;
+let isLeader = true;
+// if (isLeader===true){
+//     prodPrice = 0;
+//     console.log("E leader aise, taka nis na!")
+// } else {
+//     prodPrice = prodPrice+100;
+//     console.log(prodPrice);
+// }
+
+// prodPrice=isLeader===true?0:prodPrice+100;
+
+// Assign Value:
+// variable = condition ? valueIfTrue : valueIfFalse;
+// Log
+// console.log(condition ? valueIfTrue : valueIfFalse);
+
+console.log(isLeader === true ? "E leader aise, taka nis na" : prodPrice + 100);
+
+// Practice: Page 90
+// 1
+// Ternary
+
+const customerBuyingPrice = 4000;
+const cashback = 500;
+console.log(customerBuyingPrice > 3000 ? cashback + " " + "Taka Cashback" : "No Cashback");
+
+// if(customerBuyingPrice>3000){
+//     console.log(cashback);
+// } else {
+//     console.log("No cashback");
+// }
+
+
+// 2
+let yourAge = 14;
+console.log(yourAge < 15 ? "Child" : (yourAge >= 15 && yourAge <= 19 ? "Teenager" : "Buira Beta"));
+
+// 3
+let isLoggedIn = true;
+console.log(isLoggedIn == true ? "Welcome back" : "Please login");
+
+// 4
+let tankIsFull = true;
+console.log(tankIsFull == true ? "Ready for a long drive" : "Fill the tank");
+
+// 5
+let passInExam = false;
+console.log(passInExam === true ? "Party time" : "Next semester e serious study korbo");
+
+// 6
+let sunny = false;
+console.log(sunny === false ? "Stay Home" : "Let's Play");
+
+// 7
+let expensive = false;
+console.log(expensive==false? "I will buy this item!" : "Not gonna buy!");
